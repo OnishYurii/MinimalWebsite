741dc86e6d9c918c67a2341c0b8c4da2ffeda0ab
OnishYurii <onishchenkoyurii@gmail.com>
2023-08-15 10:57:05 +0300

    updated

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,13 +3,23 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    burgerMenu: document.querySelector('.mob-nav-menu')
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.burgerMenu.addEventListener('click', toggleBurgerMenu)
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     document.body.classList.toggle('no-scroll');
+  }
+  function toggleBurgerMenu(e) {
+    if (e.target.nodeName !== "A") {
+      return
+    } else {
+      refs.menu.classList.toggle('is-hidden');
+      document.body.classList.toggle('no-scroll');
+    }
   }
 })();
